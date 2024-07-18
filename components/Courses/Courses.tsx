bb3c337ad4b1c1ad9e7922e2afb475e857ae0d9f
Clarawita <clarawita28@gmail.com>
2024-07-18 22:24:46 +0700

    feat(courses): style course page

--- components/Courses/Courses.tsx
+++ components/Courses/Courses.tsx
@@ -44,39 +44,51 @@ const Courses: React.FC = () => {
   }, []);
 
   return (
-    <div className="flex flex-col gap-16 pt-36 justify-center items-center w-11/12 mx-auto">
+    <div className="flex flex-col gap-16 pt-36 pb-16 justify-center items-center w-11/12 mx-auto">
       <h1 className="font-bold text-3xl">Courses</h1>
       {courses.length === 0 ? (
         <p>Loading courses...</p>
       ) : (
         <ul className="grid grid-cols-4 gap-5">
           {courses.map((course) => (
-            <li className="bg-violet-200 flex flex-col gap-4 " key={course.id}>
+            <li
+              className="bg-violet-200 flex flex-col gap-4 pt-5 p-4 rounded-lg"
+              key={course.id}
+            >
               <div className="flex flex-col items-center gap-2">
-                <h2 className="">{course.course}</h2>
+                <h2 className="font-bold text-lg text-center h-14 ">
+                  {course.course}
+                </h2>
                 <Image
                   src={course.imgUrl}
                   alt={course.course}
-                  width={250}
-                  height={150}
+                  width={200}
+                  height={200}
                 />
                 <p>{course.description}</p>
               </div>
-              <div>
-                <h3>Topics:</h3>
-                <ul>
-                  {course.topics.map((topic) => (
-                    <li key={topic.topicId}>
-                      <strong>{topic.name}</strong>: {topic.content}
-                    </li>
-                  ))}
-                </ul>
-                <h3>Lecturers:</h3>
-                <ul>
-                  {course.lecturers.map((lecturer, index) => (
-                    <li key={index}>{lecturer}</li>
-                  ))}
-                </ul>
+              <div className="flex flex-col justify-between h-full">
+                <div>
+                  <h3 className="font-bold">Topics:</h3>
+                  <ul>
+                    {course.topics.map((topic) => (
+                      <li
+                        className="list-decimal list-inside"
+                        key={topic.topicId}
+                      >
+                        <strong className="text-base">{topic.name}</strong>: <p className="text-sm text-gray-700">{topic.content}</p>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+                <div>
+                  <h3 className="font-bold pt-4">Lecturers:</h3>
+                  <ul>
+                    {course.lecturers.map((lecturer, index) => (
+                      <li className="list-disc list-inside" key={index}>{lecturer}</li>
+                    ))}
+                  </ul>
+                </div>
               </div>
             </li>
           ))}
